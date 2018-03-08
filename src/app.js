d789565fb35c6a594aad4bf70dff7a749ad37fee
Wojciech Dudek <wdudek82@gmail.com>
2018-03-08 22:46:24 +0100

    Initial

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecissionApp';
+import 'normalize.css/normalize.css';
+import './scss/main.scss';
+
+ReactDOM.render(<IndecisionApp />, root);
