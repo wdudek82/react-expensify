abc7e887b1882c5481b8456bceabf3b394f97c1c
Wojciech Dudek <wdudek82@gmail.com>
2018-03-11 15:39:40 +0100

    Added babel plugin transform-object-rest-spred

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,51 +1,5 @@
 import { createStore, combineReducers } from 'redux';
-
-// Actions needed
-// ADD_EXPENSE
-// REMOVE_EXPENSE
-// EDIT_EXPONSE
-// ---------------
-// SET_TEXT_FILTER
-// SORT_BY_DATE
-// SORT_BY_AMOUNT
-// SET_START_DATE
-// SET_END_DATE
-
-// Reducers
-const expensesReducerDefaultState =[];
-const expensesReducer = (state = expensesReducerDefaultState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
-
-const filterReducerDefaultState = {
-  text: '',
-  sortBy: 'date', // date of amount
-  startDate: undefined,
-  endDate: undefined
-};
-const filterReducer = (state = filterReducerDefaultState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
-
-// Store creation
-const store = createStore(
-  combineReducers({
-    expenses: expensesReducer,
-    filters: filterReducer
-  })
-);
-
-const unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
-});
-
-console.log(store.getState());
+import uuid from 'uuid';
 
 const demoState = {
   expensess: [{
@@ -63,5 +17,144 @@ const demoState = {
   }
 };
 
+// =============== Expense Actions
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
 
-console.log('redux-expensify started..');
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+const editExpense = (
+  {
+    id,
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  }
+) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  description,
+  note,
+  amount,
+  createdAt
+});
+
+// =============== Filter Actions
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// =============== Expense Reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'EDIT_EXPENSE':
+      const updatedExpense = {
+        description: action.description,
+        note: action.note,
+        amount: action.amount,
+        createdAt: action.createdAt
+      };
+      return [
+        ...state.filter(({ id }) => id !== action.id),
+        updatedExpense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    default:
+      return state;
+  }
+};
+
+// =============== Filter Reducer
+const filterReducerDefaultState = {
+  text: '',
+  sortBy: 'date', // date of amount
+  startDate: undefined,
+  endDate: undefined
+};
+const filterReducer = (state = filterReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// =============== Store creation
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filterReducer
+  })
+);
+
+// =============== Store subscrbe
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// =============== Dispatching actions
+const expenseOne = store.dispatch(addExpense({
+  description: 'January Rent',
+  note: 'This was the final payment for that address',
+  amount: 54500,
+  createdAt: 0
+}));
+
+const expenseTwo = store.dispatch(addExpense({
+  description: 'February Rent',
+  note: 'This was new payment for that address',
+  amount: 244,
+  createdAt: 0
+}));
+
+store.dispatch(
+  removeExpense({ id: expenseOne.expense.id })
+);
+
+store.dispatch(
+  editExpense(
+    { 
+      id: expenseTwo.expense.id,
+      description: 'Coffee',
+      note: 'One cup of delicious coffee',
+      amount: 299,
+      createdAt: 555
+    })
+);
+
+console.log(expenseOne.expense.id);
+
+
+const user = {
+  name: 'Jen',
+  age: 24
+};
+
+console.log({ ...user });
