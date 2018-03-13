3991d803520486fd7b1f510aa3e2221b7b306ab7
Wojciech Dudek <wdudek82@gmail.com>
2018-03-13 22:24:12 +0100

    Added remove expense functionality to ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div key={id}>
     {console.log(id)}
     <h3>{description}</h3>
     <p>{amount} - created: {createdAt}</p>
+    <button onClick={(e) => {
+      dispatch(
+        removeExpense({ id })
+      );
+    }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
