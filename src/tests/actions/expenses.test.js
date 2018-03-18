6296b435a64e8de23a29d75f5e0b3aedffe20f5a
Wojciech Dudek <wdudek82@gmail.com>
2018-03-18 15:30:50 +0100

    Added tests for redux expense actions

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,52 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should setup add expense action object with provided values', () => {
+  const expenseData = {
+    id: expect.any(String),
+    description: 'Rent',
+    amount: 109500,
+    createdAt: 1000,
+    note: 'This was last months rent'
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData
+    }
+  });
+});
+
+test('should setup add expense action object with default values', () => {
+  const action = addExpense();
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: expect.any(String),
+      description: '',
+      amount: 0,
+      createdAt: 0,
+      note: ''
+    }
+  });
+});
+
+test('should setup edit expense action object', () => {
+  const updates = { note: 'New note value' };
+  const action = editExpense('abc123', updates);
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: 'abc123',
+    updates: {
+      note: 'New note value'
+    }
+  });
+});
+
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: 'abc123' });
+  expect(action).toEqual({
+    type: 'REMOVE_EXPENSE',
+    id: 'abc123',
+  });
+});
