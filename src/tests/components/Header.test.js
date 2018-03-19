3fbac9ccd04e9d8cb53f8f2815deb5092398c3d7
Wojciech Dudek <wdudek82@gmail.com>
2018-03-19 22:36:09 +0100

    Added jest config for enzyme and enzyme-to-json

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
+import HelpPage from '../../components/HelpPage';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
-  
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+
+  // Enzyme
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
+
+  // react-test-render
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
   // console.log(renderer.getRenderOutput());
 });
