385bb09de09efcf90591f66a3b90120407694b14
Wojciech Dudek <wdudek82@gmail.com>
2018-03-19 22:02:27 +0100

    Added Enzyme configuration, and one test for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-  <div>
+  <header>
     <h1>Expensify</h1>
     <NavLink exact to="/" activeClassName="nav-active">Dashboard</NavLink>
     <NavLink to="/create" activeClassName="nav-active">Create Expense</NavLink>
     <NavLink to="/help" activeClassName="nav-active">Help</NavLink>
-  </div>
+  </header>
 );
 
 export default Header;
