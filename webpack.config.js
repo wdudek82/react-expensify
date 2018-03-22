b4123777b122d416973f04f59a77644bf37c2d4a
Wojciech Dudek <wdudek82@gmail.com>
2018-03-22 18:37:17 +0100

    Fixed webpack config, and setup css maps + extractying css in prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,49 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
-    mode: 'development',  
+    // mode: 'development',  
     output: {
       path: path.join(__dirname, 'public'),
       filename: 'bundle.js'
     },
     module: {
       rules: [
-        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
         {
           test: /\.s?css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-          ]
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         },
       ]
     },
-    devtool: isProduction ? 'source-map' : 'chip-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       compress: true,
