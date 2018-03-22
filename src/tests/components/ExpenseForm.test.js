57a432e6b470206c2ccb9985ed3b79774400d11d
Wojciech Dudek <wdudek82@gmail.com>
2018-03-22 17:46:31 +0100

    New webpack config (as a function), added tests for dynamic component ExpenseForm

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,8 +1,93 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
+import expenses from '../fixtures/expenses';
 import ExpenseForm from '../../components/ExpenseForm';
 
 it('should render ExpenseForm correctly', () => {
   const wrapper = shallow(<ExpenseForm />);
   expect(wrapper).toMatchSnapshot();
+});
+
+it('should render ExpenseForm correctly with expense data', () => {
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  // looking by tag name
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should set description on input change', () => {
+  const value = 'New description';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(0).simulate('change', {
+    target: { value }
+  });
+  expect(wrapper.state('description')).toBe(value);
+});
+
+it('should set note on input change', () => {
+  const value = 'New note value';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('textarea').simulate('change', {
+    target: { value }
+  });
+  expect(wrapper.state('note')).toBe(value);
+});
+
+it('should set amount for valid input', () => {
+  const value = '100.25';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: { value }
+  });
+  expect(wrapper.state('amount')).toBe(value);
+});
+
+it('should not set amount for invalid input', () => {
+  const value = '12.122';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: value
+  });
+  expect(wrapper.state('amount')).toBe();
+});
+
+it('should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />);
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
+  expect(wrapper.state('error')).toBe('');
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  });
+  // onSubmitSpy('Andrew', 'Philadelphia');
+  // expect(onSubmitSpy).toHaveBeenCalledWith('Andrew', 'Philadelphia');
+});
+
+it('should set new date on date change', () => {
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+  expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+it('should set calendar focus on change', () => {
+  const focused = true;
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused });
+  expect(wrapper.state('calendarFocused')).toBe(focused);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,34 @@
 const path = require('path');
 
-console.log(path.join(__dirname, 'public'));
+module.exports = (env) => {
+  const isProduction = env === 'production';
 
-module.exports = {
-  entry: './src/app.js',
-  mode: 'development',  
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-    ]
-  },
-  devtool: 'chip-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 8000,
-    historyApiFallback: true
+  return {
+    entry: './src/app.js',
+    mode: 'development',  
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'chip-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      compress: true,
+      port: 8000,
+      historyApiFallback: true
+    }
   }
 };
