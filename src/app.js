b3350d668ff416be8b2624ed4333936ffa3457f5
Wojciech Dudek <wdudek82@gmail.com>
2018-03-25 00:14:42 +0100

    Indegrated Firebase

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ import 'normalize.css/normalize.css';
 import './scss/main.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
+import './firebase/firebase';
+
 const store = configureStore();
 
 store.dispatch(
---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyACgCaSjEJeF9pQDxN6dtKI8p-uijV8MHY",
+  authDomain: "expensify-b2882.firebaseapp.com",
+  databaseURL: "https://expensify-b2882.firebaseio.com",
+  projectId: "expensify-b2882",
+  storageBucket: "expensify-b2882.appspot.com",
+  messagingSenderId: "915990714023"
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+  name: 'Wojtek Dudek'
+});
